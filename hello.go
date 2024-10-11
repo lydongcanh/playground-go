0912ddfc2af783658cbf52b3bc5951e284aab5b5
Ted Ly <lydongcanh@gmail.com>
2024-10-11 08:54:53 +0700

    Make simple post request to get training plan.

--- hello.go
+++ hello.go
@@ -1,20 +1,56 @@
 package main
 
 import (
-	"errors"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 )
 
 func main() {
-	fmt.Println(Hello("Ted"))
-	fmt.Println(Hello(""))
-}
-
-func Hello(name string) (string, error) {
-	if name == "" {
-		return "", errors.New("empty name")
+	request := GymTrainingPlanRequest{
+		FitnessLevel:           1,
+		TrainingGoal:           1,
+		TimeCommitmentInMinute: 60,
+		AdditionalContext:      "Make it all legs",
 	}
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message, nil
+	trainingPlan, trainingPlanError := GetGymTrainingPlan(request)
+	if trainingPlanError != nil {
+		fmt.Println("Error getting training plan:", trainingPlanError)
+		return
+	}
+
+	fmt.Println(trainingPlan)
+}
+
+type GymTrainingPlanRequest struct {
+	FitnessLevel           int    `json:"fitnessLevel"`
+	TrainingGoal           int    `json:"trainingGoal"`
+	TimeCommitmentInMinute int    `json:"timeCommitmentInMinute"`
+	AdditionalContext      string `json:"additionalContext"`
+}
+
+func GetGymTrainingPlan(request GymTrainingPlanRequest) (string, error) {
+	requestJson, rerequestJsonError := json.Marshal(request)
+	if rerequestJsonError != nil {
+		fmt.Println("Error marshalling request to JSON:", rerequestJsonError)
+		return "", rerequestJsonError
+	}
+
+	response, responseError := http.Post("https://playground-core.onrender.com/llmrecommendation/gym-training-plan", "application/json", bytes.NewBuffer(requestJson))
+	if responseError != nil {
+		fmt.Println("Error sending request:", responseError)
+		return "", responseError
+	}
+	defer response.Body.Close()
+
+	body, bodyError := io.ReadAll(response.Body)
+	if bodyError != nil {
+		fmt.Println("Error reading response body:", bodyError)
+		return "", bodyError
+	}
+
+	return string(body), nil
 }
